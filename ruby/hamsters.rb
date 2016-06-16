6652935fdbc636fb03f2d08ae2e04dac5f468803
Meric BAL <MB@Meric-MacBook-Air.local>
2016-06-16 14:58:52 -0700

    Questions added !

---
+++ ruby/hamsters.rb
@@ -0,0 +1,14 @@
+puts "Hamster's name ?"
+name = gets.chomp
+
+puts "What is the volume level of hamster ? 1 to 10 ?"
+volume = gets.chomp.to_i
+
+puts "What is the fur color of hamster ?"
+color = gets.chomp
+
+puts "Is the hamster good for adoption ?"
+adoption = gets.chomp
+
+puts "Age of the hamster ?"
+age = gets.chomp.to_i
