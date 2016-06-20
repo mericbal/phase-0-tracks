57ff5b70f2144e94bd9309d59c4eb3e96e28bf04
Meric BAL <MB@Meric-MacBook-Air.local>
2016-06-20 00:16:48 -0700

    Decrypt created !

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,7 +1,7 @@
 
 i = 0
 
-puts 'Enter a string ..'
+puts 'Enter a string for encription ..'
 string = gets.chomp
 
 while i < string.length
@@ -13,6 +13,30 @@ while i < string.length
   else string[i] = string[i].next
   end
 
+  i += 1
+end
+
+puts string
+
+
+
+
+
+
+i = 0
+
+puts 'Enter a string for decription ..'
+string = gets.chomp
+alp = 'abcdefghijklmnopqrstuvwxyz'
+
+while i < string.length
+  if string[i] == 'a'
+    string[i] = 'z'
+  elsif string[i] == ' '
+    string[i] = ' '
+  else
+    string[i] = alp[alp.index(string[i])-1]
+  end
   i += 1
 end
 
