ccd2343bfc0ef5c4034ef0e646019b5e73e80c33
Meric BAL <MB@Meric-MacBook-Air.local>
2016-06-19 22:02:55 -0700

    Improvements made !

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,13 +1,18 @@
+
 i = 0
 
 puts 'Enter a string ..'
 string = gets.chomp
 
 while i < string.length
-  string[i] = string[i].next
-  if string[i] == '!'
+
+  if string[i] == ' '
     string[i] = ' '
+  elsif string[i] == 'z'
+    string[i] = 'a'
+  else string[i] = string[i].next
   end
+
   i += 1
 end
 
