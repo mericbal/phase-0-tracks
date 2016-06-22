47f54c6102678aae8396b634768e3be90f09db87
Meric BAL <MB@Meric-MacBook-Air.local>
2016-06-22 15:57:55 -0700

    Done !

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,23 +1,33 @@
 freeway = {
-    car1 = {
+    car1: {
         make: 'BMW',
         year: 2016,
         passengers: 2,
         speed: {
             max_speed: 140,
             ave_speed: 70,
-        }
-        names: ['Meric', 'Nil']
-    }
+        },
+        passenger_names: ['Meric', 'Nil'],
+    },
 
-    car2 = {
+    car2: {
         make: 'Lexus',
         year: 2014,
         passengers: 3,
         speed: {
             max_speed: 120,
             ave_speed: 80,
-        }
-        names: ['Kaan', 'Berker', 'Ugur']
-    }
+        },
+        passenger_names: ['Kaan', 'Berker', 'Ugur'],
+    },
 }
+
+p freeway[:car1]
+
+p freeway[:car2]
+
+p freeway[:car1][:speed][:max_speed]
+
+p freeway[:car2][:passenger_names][0]
+
+p freeway[:car1][:passengers]
