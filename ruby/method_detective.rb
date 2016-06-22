f2aedfc365970ce2dfe05b4e0e17211244645207
Meric BAL <MB@Meric-MacBook-Air.local>
2016-06-21 18:16:33 -0700

    Done !

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -26,7 +26,7 @@
 "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!".squeeze
