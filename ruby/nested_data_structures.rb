67cf3cedf2166e04ccfc8aa67b643d3384a90414
Meric BAL <MB@Meric-MacBook-Air.local>
2016-06-22 15:45:22 -0700

    Cars added !

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,23 @@
+freeway = {
+    car1 = {
+        make: 'BMW',
+        year: 2016,
+        passengers: 2,
+        speed: {
+            max_speed: 140,
+            ave_speed: 70,
+        }
+        names: ['Meric', 'Nil']
+    }
+
+    car2 = {
+        make: 'Lexus',
+        year: 2014,
+        passengers: 3,
+        speed: {
+            max_speed: 120,
+            ave_speed: 80,
+        }
+        names: ['Kaan', 'Berker', 'Ugur']
+    }
+}
