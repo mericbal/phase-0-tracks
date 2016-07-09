0ab6408b8a3d29c63ba538e456d434c5db687675
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-09 14:11:47 -0700

    Rel 0 started !

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,16 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+    def fetch(toy)
+        puts "I brought back the #{toy} !"
+        toy
+    end
+
+    def speak(x)
+        x.times { p 'Woof !'}
+    end
+
 
 end
 
+kuti = Puppy.new
+kuti.fetch('ball')
