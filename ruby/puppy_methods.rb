1b0d2751b7b00d8691d3edf2be8516d06fd3fa3c
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-09 14:18:22 -0700

    Rel 0 is done !

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -9,8 +9,23 @@ class Puppy
         x.times { p 'Woof !'}
     end
 
+    def roll_over
+        puts ' * rolls over * !'
+    end
+
+    def dog_years(x)
+        p (x / 7)
+    end
+
+    def treat(treat)
+        puts 'Puppy loved ' + treat + ' !'
+    end
 
 end
 
 kuti = Puppy.new
 kuti.fetch('ball')
+kuti.speak(7)
+kuti.roll_over
+kuti.dog_years(49)
+kuti.treat('steak')
