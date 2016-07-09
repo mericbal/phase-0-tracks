5fc2b6b6a914ca66ee1e2ec2ce2f4ae1934ce566
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-09 15:13:37 -0700

    Rel 2 is added !

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -27,9 +27,43 @@ class Puppy
 
 end
 
+
 kuti = Puppy.new
 kuti.fetch('ball')
 kuti.speak(7)
 kuti.roll_over
 kuti.dog_years(49)
 kuti.treat('steak')
+
+
+
+
+
+
+game = []
+
+class Euro2016
+
+    def initialize
+        p 'Game started ! '
+    end
+
+    def france(player, min)
+        p 'Goaall ! France scored ! ' + player.to_s.capitalize + ' in ' + min.to_s + 'th minutes . '
+    end
+
+    def germany(player, min)
+        p 'Goaall ! Germany scored ! ' + player.to_s.capitalize + ' in ' + min.to_s + 'th minutes . '
+    end
+
+
+end
+
+
+50.times do
+    match = Euro2016.new
+    game.push(match.france('payet',21))
+    game.push(match.germany('muller',32))
+end
+
+p game
