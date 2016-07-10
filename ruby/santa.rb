d67934b5579ceb57a5717dafb70030ff65477a07
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-10 00:38:42 -0700

    Rel 3 is added !

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,8 @@
+
+
 class Santa
+    attr_reader :ethnicity, :age
+    attr_accessor :gender
 
     def speak
         p 'Ho ho ho ! Happppyyy Hoolliidaysss !'
@@ -8,13 +12,54 @@ class Santa
         p 'That was a good ' + cookie.to_s + ' cookie !'
     end
 
-    def initialize
+    def initialize(gender, ethnicity)
         p 'Initializing Santa instance ..'
+        @gender = gender
+        @ethnicity = ethnicity
+        @age = 0
     end
 
 
+    def birthday
+        @age = @age + 1
+    end
+
+    def get_mad(input)
+        ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+        ranking.delete(input)
+        ranking.push(input)
+        ranking
+    end
+
+    # def gender=(new_gender)
+    #     @gender = new_gender
+    # end
+    #
+    # def gender
+    #     @gender
+    # end
+    #
+    # def age
+    #     @age
+    # end
+    #
+    # def ethnicity
+    #     @ethnicity
+    # end
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies('chocolate')
+santa1 = Santa.new('Male', 'Hispanic')
+santa1.speak
+santa1.eat_milk_and_cookies('chocolate')
+
+p santa1.gender
+santa1.gender=('Female')
+p santa1.gender
+
+
+santa1.get_mad('Comet')
+p santa1.age
+santa1.birthday
+p santa1.age
+
+p santa1.ethnicity
