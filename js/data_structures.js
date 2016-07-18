10f4312a3f36312e73e5ab6f1241aca9aca96cab
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-17 23:20:01 -0700

    Added Rel 1.

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,7 +2,7 @@ var colors = ['blue', 'gray', 'black', 'brown']
 var names = ['Ed', 'Ted', 'Zed', 'Chad']
 
 
-function add_horse(name) {
+function add_name(name) {
     names.push(name)
     names
 }
@@ -13,10 +13,19 @@ function add_color(color) {
 }
 
 console.log(names)
-add_horse('Med')
+add_name('Med')
 console.log(names)
 
 
 console.log(colors)
 add_color('zebra')
 console.log(colors)
+
+
+var random_name = names[Math.floor((Math.random() * names.length))]
+var random_color = colors[Math.floor((Math.random() * colors.length))]
+
+var horse1 = {}
+horse1[random_name] = random_color
+
+console.log(horse1)
