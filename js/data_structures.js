f65c855a4558ec2c5abd5e2ad73d5e1414e0eb04
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-17 22:35:02 -0700

    File created and arrays added !

---
+++ js/data_structures.js
@@ -0,0 +1,13 @@
+var colors = ['blue', 'gray', 'black', 'brown']
+var names = ['Ed', 'Ted', 'Zed', 'Chad']
+
+
+function add_horse(name) {
+    names.push(name)
+    names
+}
+
+console.log(names)
+
+add_horse('Med')
+console.log(names)
