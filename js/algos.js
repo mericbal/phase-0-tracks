7c766d9ded56e6a875eb001ca81d842aefa60c0e
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-18 01:27:21 -0700

    Got stuck on Rel 2 !

--- js/algos.js
+++ js/algos.js
@@ -12,21 +12,49 @@
 // longest(words)
 
 
+//
+//
+// var guy1 = {name: 'Steven', age:54}
+// var guy2 = {name: 'Tamir', age:54}
+//
+// console.log(guy1.age)
+//
+// function compare(obj1,obj2) {
+//     if (obj1.age == obj2.age) {
+//         console.log('true')
+//     } else if (obj1.name == obj2.name) {
+//         console.log('true')
+//     } else {
+//         console.log('false')
+//     }
+// }
+//
+// (compare(guy1,guy2))
 
 
-var guy1 = {name: 'Steven', age:54}
-var guy2 = {name: 'Tamir', age:54}
-
-console.log(guy1.age)
-
-function compare(obj1,obj2) {
-    if (obj1.age == obj2.age) {
-        console.log('true')
-    } else if (obj1.name == obj2.name) {
-        console.log('true')
-    } else {
-        console.log('false')
+function random(x) {
+    var string = ''
+    for (var i = 0; i < x; i++ ) {
+    var letter = alp[Math.floor((Math.random() * alp.length))]
+    string += letter
     }
+    console.log(string)
 }
 
-(compare(guy1,guy2))
+// console.log(string);
+
+var alp = 'abcdefghijklmnoprstquvwyxz'
+//
+random()
+//
+// function length()
+//
+
+// var random = alp[Math.floor((Math.random() * alp.length))]
+//
+//
+//
+// console.log(random)
+
+// var meric = '77' + random + random +random
+// console.log(meric + random);
