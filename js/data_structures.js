97e0d4438e72d3af3202d4af346e69ffd28fb934
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-17 23:30:47 -0700

    Added Rel 2 .

--- js/data_structures.js
+++ js/data_structures.js
@@ -29,3 +29,22 @@ var horse1 = {}
 horse1[random_name] = random_color
 
 console.log(horse1)
+
+
+
+
+
+
+
+var car_makes = ['Lexus', 'Bmw', 'Mercedes', 'Maserati']
+var horsepowers = [150, 210, 180, 240]
+
+function car_factory() {
+    var car1 = {}
+    var car_make = car_makes[Math.floor((Math.random() * car_makes.length))]
+    var horsepower = horsepowers[Math.floor((Math.random() * horsepowers.length))]
+    car1[car_make] = horsepower
+    console.log(car1)
+}
+
+car_factory()
