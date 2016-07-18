16458698692ff6601c67b0a1279e59b26b15b5c7
Meric BAL <MB@Meric-MacBook-Air.local>
2016-07-18 00:33:34 -0700

    Added Rel 1.

--- js/algos.js
+++ js/algos.js
@@ -1,12 +1,32 @@
-var words = ["long phrase","longest phrase","longer phrase",'llloooooooooooooooooooooonnnnggg!!!', 'nooottt veery long']
+// var words = ["long phrase","longest phrase","longer phrase",'llloooooooooooooooooooooonnnnggg!!!', 'nooottt veery long']
+//
+// function longest(array) {
+//     longest = ''
+//     for (var i = 0; i < array.length; i++ ) {
+//         if (array[i].length > longest.length) {
+//             longest = array[i]
+//         }
+//     } console.log(longest)
+// }
+//
+// longest(words)
 
-function longest(array) {
-    longest = ''
-    for (var i = 0; i < array.length; i++ ) {
-        if (array[i].length > longest.length) {
-            longest = array[i]
-        }
-    } console.log(longest)
+
+
+
+var guy1 = {name: 'Steven', age:54}
+var guy2 = {name: 'Tamir', age:54}
+
+console.log(guy1.age)
+
+function compare(obj1,obj2) {
+    if (obj1.age == obj2.age) {
+        console.log('true')
+    } else if (obj1.name == obj2.name) {
+        console.log('true')
+    } else {
+        console.log('false')
+    }
 }
 
-longest(words)
+(compare(guy1,guy2))
